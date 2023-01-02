ec1c60fe140885d04c271a56d7c9f5c3a7dd324e
ronitshinde <rony.shinde753@gmail.com>
2023-01-02 16:49:38 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RefactorEmpWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+
+}
