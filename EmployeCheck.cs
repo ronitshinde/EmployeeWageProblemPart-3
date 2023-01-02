f661196e84bf41c3eceecafe32b51961f46874cd
ronitshinde <rony.shinde753@gmail.com>
2023-01-02 17:11:50 +0530

    Code to check employee attendance

---
+++ EmployeCheck.cs
@@ -0,0 +1,26 @@
+﻿namespace RefactorEmpWage
+{
+    public class EmployeCheck
+    {
+        public static void EmpAttendance()
+        {
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+            Console.WriteLine(empcheck);
+
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+
+    }
+}
+
