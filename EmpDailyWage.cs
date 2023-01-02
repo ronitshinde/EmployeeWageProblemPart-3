de5cc60105c71443dfa2374225f133e46a146612
ronitshinde <rony.shinde753@gmail.com>
2023-01-02 17:43:36 +0530

    Code to check Daily wages of employee

---
+++ EmpDailyWage.cs
@@ -0,0 +1,34 @@
+﻿namespace RefactorEmpWage
+{
+    public class EmpDailyWage
+    {
+
+        public static void EmpWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+
+            int Empwage;
+            int Emphours;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+                Emphours = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                Emphours = 0;
+            }
+            Empwage = Emphours * Emp_Rate_Per_Hour;
+
+            Console.WriteLine("Daily employee wage is : " + Empwage);
+
+
+        }
+    }
+}
