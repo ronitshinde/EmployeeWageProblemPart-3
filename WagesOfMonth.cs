d9ac4c8249a30ac3a868cd1a7d83c9e95cd7da7c
ronitshinde <rony.shinde753@gmail.com>
2023-01-04 15:24:14 +0530

    Code to calculate monthly wages

---
+++ WagesOfMonth.cs
@@ -0,0 +1,50 @@
+﻿
+namespace RefactorEmpWage
+{
+    public class WagesOfMonth
+    {
+        public const int Is_Full_Time = 1;
+        public const int Is_Part_Time = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 20;
+
+
+        public static void EmployeeMonthlyWage()
+        {
+
+            int Empwage;
+            int Emphours;
+            int TotalEmpwage = 0;
+
+            for (int day = 0; day < Num_Of_Working_Days; day++)
+            {
+                Random random = new Random();
+                int empcheck = random.Next(3);
+
+
+                switch (empcheck)
+                {
+                    case Is_Full_Time:
+                        Emphours = 8;
+                        break;
+
+                    case Is_Part_Time:
+                        Emphours = 4;
+                        break;
+
+                    default:
+                        Emphours = 0;
+                        break;
+                }
+
+                Empwage = Emphours * Emp_Rate_Per_Hour;
+                TotalEmpwage = TotalEmpwage + Empwage;
+                Console.WriteLine("Daily employee wage is : " + Empwage);
+            }
+
+            Console.WriteLine("Tota Emp wage : " + TotalEmpwage);
+
+
+        }
+    }
+}
